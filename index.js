d861cec8945f4a6c4e040142c5239a649155d0a8
binish900 <159807924+binish900@users.noreply.github.com>
2024-03-26 04:17:40 -0700

    Add files via upload
    CLI calculator with myself

---
+++ index.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "enter first number", type: "number", name: "firstnumber" },
+    { message: "enter second number", type: "number", name: "secondnumber" },
+    {
+        message: "select one of the operators to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "substraction", "multiplication", "division"],
+    },
+]);
+console.log(answer);
+if (answer.operator === "addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "substraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log("please select valid operator");
+}
